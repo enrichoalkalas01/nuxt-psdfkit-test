6888beae09bb1e329a63e52c7d9d3fb7e6801b38
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-04-20 15:11:58 +0700

    update range on foto data

--- src/components/pencarian/Foto.vue
+++ src/components/pencarian/Foto.vue
@@ -4,7 +4,7 @@
             <SplideSlide
                 v-for="(foto, i) in fotos" :key="i"
             >
-                <div class="col-12 col-md-9 text-center">
+                <div class="box-image col-12 col-md-9 text-center">
                     <a :href="`/foto-detail/${ foto.document_id }`">
                         <img :src="`${ this.$store.state.Tools.GetUrlFiles + foto.preview }`" class="img-square32 rounding" alt="">
                     </a>
@@ -35,8 +35,9 @@
                 SliderConfig: {
                     updateOnMove: true,
                     type: 'loop',
-                    focus: 'center',
-                    perPage: 2,
+                    // focus: 'center',
+                    rewind : true,
+                    perPage: 3,
                     pagination: false
                 },
                 fotos: null,
@@ -55,3 +56,18 @@
         },
     }
 </script>
+
+<style>
+    .splide__slide .box-image {
+        margin: auto;
+        width: 90%;
+    }
+
+    #splide01 .splide__arrow--next {
+        right: -1em !important;
+    }
+
+    #splide01 .splide__arrow--prev {
+        left: -1em !important;
+    }
+</style>
