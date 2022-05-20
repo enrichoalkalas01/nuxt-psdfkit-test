66dd138ae5bfc6042c21edab36b055bc6a6d148b
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-05-20 15:28:13 +0700

    change user to logout

--- src/components/dashboard/SideBar.vue
+++ src/components/dashboard/SideBar.vue
@@ -28,29 +28,23 @@
                 <li>
                     <a href="#" class="nav-link text-white">
                     <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
-                        Products
+                        Akun
                     </a>
                 </li>
                 <li>
                     <a href="#" class="nav-link text-white">
                     <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
-                        Customers
+                        Pesan
                     </a>
                 </li>
             </ul>
             <hr>
             <div class="dropdown">
-                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                    <strong>mdo</strong>
+                <a href="/logout" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <strong>Logout</strong>
+                    <!-- <img src="https://github.com/mdo.png" alt="" width="25" height="25" class="rounded-circle me-2 mr-2">
+                    <strong>{{ this.$store.state.Login.UserData.username }}</strong> -->
                 </a>
-                <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                    <li><a class="dropdown-item" href="#">New project...</a></li>
-                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Sign out</a></li>
-                </ul>
             </div>
         </div>
     </section>
