488c0829ce3fbcc9e2c0ae2726d9472733a99ef6
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-05-23 10:37:17 +0700

    change dashboard layout

--- src/components/dashboard/MainPage.vue
+++ src/components/dashboard/MainPage.vue
@@ -1,6 +1,5 @@
 <template>
     <section id="dashboard" class="container-fluid">
-        <TopBarReflection />
         <div class="row box-dashboard">
             <SideBar />
             <div class="col-9 py-3 box-cd" id="box-cd">
@@ -11,12 +10,10 @@
 </template>
 
 <script>
-    import TopBarReflection from './TopBarReflection.vue'
     import SideBar from './SideBar.vue'
     export default {
         name: "MainPage",
         components: {
-            TopBarReflection,
             SideBar
         },
         
