792f8891f3069aac188bc118ab080e3f12b333a9
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-05-24 15:45:36 +0700

    Update SideBar.vue

--- src/components/dashboard/SideBar.vue
+++ src/components/dashboard/SideBar.vue
@@ -38,7 +38,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link text-white">
+                    <a href="/dashboard/pesan" class="nav-link text-white">
                     <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
                         Pesan
                     </a>
