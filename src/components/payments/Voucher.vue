e21e905d88d8318faf12f38e91274f7b7d282b53
Aldi Fathur <aldi.fathur@kompas.com>
2022-05-25 09:46:22 +0700

    redeem voucher

--- src/components/payments/Voucher.vue
+++ src/components/payments/Voucher.vue
@@ -13,8 +13,8 @@
                     <div class="col-12 tab-pane fade show active" id="bukuTabs02" role="tabpanel" aria-labelledby="buku-Tabs02">
                         <div class="wrapper-payment-voucher">
                             <span>Masukkan Kode Akses KompasData</span>
-                            <input type="text" class="form-control mb-2">
-                            <button class="form-control btn-success">Isi Ulang</button>
+                            <input type="text" class="form-control mb-2" id="voucher_code" v-on:keyup.enter="insertKupon">
+                            <button class="form-control btn-success" v-on:click="insertKupon(this.$store.state.Login.UserData)">Isi Ulang</button>
                         </div>
                     </div>
                 </div>
@@ -24,7 +24,31 @@
 </template>
 
 <script>
+    import Axios from 'axios'
     export default {
         name: 'Voucher',
+        methods: {
+            insertKupon: async (userdata) => {
+                var voucherCode = document.querySelector("#voucher_code").value
+                let getData = await Axios({
+                    url: `https://dev-be.kompasdata.id/api/Vouchers/${ voucherCode }/use?userid=${ userdata.id }`,
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${ userdata.token }`,
+                        'Content-Type': 'application/json'
+                    },
+                }).then( Response => Response ).catch( Error => Error );
+
+                if (getData.data) {
+                    alert(getData.data)
+                    window.location.href = '/dashboard/voucher'
+                } else if (getData.response.data.message === "Voucher number is already used."){
+                    alert("Kode voucher sudah digunakan")
+                } else {
+                    alert("Something went wrong!")
+                }
+
+            },
+        }
     }
 </script>
