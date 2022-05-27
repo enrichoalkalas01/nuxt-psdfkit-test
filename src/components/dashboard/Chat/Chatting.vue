f974e203a82b36a3c59771a5a7eea4631fde8781
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-05-27 10:01:16 +0700

    update for chat

--- src/components/dashboard/Chat/Chatting.vue
+++ src/components/dashboard/Chat/Chatting.vue
@@ -8,47 +8,91 @@
                 <div
                     v-for="(chat, i) in ChatData" :key="i"
                     :class="
-                        chat.adminId !== '' ? 'col-12 user-chat left-user' :
+                        chat.mainChat.adminId !== '' ? 'col-12 user-chat left-user' :
                         'col-12 user-chat right-user'
                     "
-                >
-                    <div class="wb-image" v-if="chat.adminId !== ''">
-                        <div class="image" style="background-image: url('https://www.kompasdata.id/Content/img/icons/cs.jpg')"></div>
-                    </div>
-                    <div class="wb-text">
-                        <div class="wp-wb-text">
-                            <!-- <div
-                                v-for="(replay, j) in ChatData" :key="j"
-                                class="replayed-message"
-                            >
-                                <span v-if="chat.parentId !== chat.id && j === ">
-                                    Replay From: {{ replay.id === chat.parentId ? replay : '' }} 
-                                </span>
-                            </div> -->
-                            <div class="title">
-                                <h5>{{ chat.title }}</h5>
+                    :id="`chatId-${ chat.mainChat.id }`"
+                >  
+                    <!-- Replay Chat -->
+                    <div
+                        class="wb-replay"
+                        v-for="(replay, j) in chat.parentChat"
+                        :key="j"
+                        :id="`replayedId-${ replay.id }`"
+                        v-on:click="goToSelectionSection( replay.id )"
+                    >
+                        <div class="wb-chat-box">
+                            <div class="wb-image" v-if="replay.adminId !== ''">
+                                <div class="image" style="background-image: url('https://www.kompasdata.id/Content/img/icons/cs.jpg')"></div>
                             </div>
-                            <div class="message">
-                                <span>{{ chat.text }}</span>
+                            <div class="wb-text">
+                                <div class="wp-wb-text">
+                                    <div class="title">
+                                        <h6>{{ replay.title }}</h6>
+                                    </div>
+                                    <div class="message">
+                                        <p>{{ replay.text }}</p>
+                                        <p>{{ replay.insertDate.substring(0, 10) }}</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="date">
-                                <span>{{ chat.insertDate.substring(0, 10) }}</span>
+                            <div class="wb-image" v-if="replay.adminId === ''">
+                                <div class="image" style="background-image: url('https://www.kompasdata.id/Content/img/icons/man.png')"></div>
                             </div>
-                            <!-- <div class="btn-action">
-                                {{ chat }}
-                            </div> -->
                         </div>
                     </div>
-                    <div class="wb-image" v-if="chat.adminId === ''">
-                        <div class="image" style="background-image: url('https://www.kompasdata.id/Content/img/icons/man.png')"></div>
+                    <!-- Real Chat -->
+                    <div class="wb-chat-box">
+                        <div class="wb-image" v-if="chat.mainChat.adminId !== ''">
+                            <div class="image" style="background-image: url('https://www.kompasdata.id/Content/img/icons/cs.jpg')"></div>
+                        </div>
+                        <div class="wb-text">
+                            <div class="wp-wb-text">
+                                <div class="title">
+                                    <h5>{{ chat.mainChat.title }}</h5>
+                                </div>
+                                <div class="message">
+                                    <span>{{ chat.mainChat.text }}</span>
+                                </div>
+                                <div class="date">
+                                    <span>{{ chat.mainChat.insertDate.substring(0, 10) }}</span>
+                                </div>
+                                <!-- <div class="btn-action">
+                                    {{ chat }}
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="wb-image" v-if="chat.mainChat.adminId === ''">
+                            <div class="image" style="background-image: url('https://www.kompasdata.id/Content/img/icons/man.png')"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-12 send-chat-box">
+            <div class="col-12 user-chat right-user" id="chatId-348">
+                <div class="wb-chat-box">
+                    <div class="wb-text">
+                        <div class="wp-wb-text">
+                            <div class="title">
+                                <h5>Testing Message</h5>
+                            </div>
+                            <div class="message">
+                                <span>ini parent nya...</span>
+                            </div>
+                            <div class="date">
+                                <span>2022-05-24</span>
+                                </div>
+                            </div>
+                        </div>
+                    <div class="wb-image">
+                        <div class="image" style="background-image: url(&quot;https://www.kompasdata.id/Content/img/icons/man.png&quot;);"></div>
+                    </div>
+                </div>
+            </div>
             <div class="wrapper-scb">
                 <textarea name="" id="" class="form-control" placeholder="Ketik untuk mengirim pesan..."></textarea>
-                <i class="fa-solid fa-paper-plane"></i>
+                <i id="send-chat" class="fa-solid fa-paper-plane"></i>
             </div>
         </div>
     </section>
@@ -73,10 +117,38 @@
             }
 
             await Axios(configChat).then(response => {
-                this.ChatData = response.data
+                // this.getReplayChat(response.data)
+                // console.log(this.getReplayChat(response.data))
+                // this.ChatData = response.data
+                this.ChatData = this.getReplayChat(response.data)
+                
             }).catch(err => {
                 console.log(err)
             })
+        },
+        methods: {
+            getReplayChat(paramChats) {
+                return paramChats.map((data, i, arrData) => {
+                    if ( data.id !== data.parentId ) return { mainChat: arrData[i], parentChat: arrData.filter(x => data.parentId === x.id ) }
+                    else return { mainChat: arrData[i], parentChat: [] }
+                    /* return { mainChat: arrData[i], parentChat: arrData.filter(x => x.id !== x.parentId && data.id === x.parentId ) // [0].id == data.id ? // arrData.filter(x => x.parentId != x.id) : [] } */
+                })
+            },
+
+            goToSelectionSection(e) {
+                var element = document.getElementById(`chatId-${ e }`);
+                var elBox = document.getElementById('box-cd')
+                var headerOffset = 100;
+                var elementPosition = element.getBoundingClientRect().top;
+                var offsetPosition = elementPosition - elBox.pageYOffset + headerOffset;
+            
+                elBox.scrollTo({
+                    top: offsetPosition,
+                    behavior: "smooth"
+                });
+            },
+
+            // sendChat
         }
     }
 </script>
