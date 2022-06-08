3f548a873bdea8e4148063f4aaaa60eb63403bf4
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-06-08 15:16:11 +0700

    Update PembayaranTab.vue

--- src/components/dashboard/Shopping/PembayaranTab.vue
+++ src/components/dashboard/Shopping/PembayaranTab.vue
@@ -6,34 +6,39 @@
                     <h4>Order Summary</h4>
                 </div>
                 <div class="col-12 item">
-                    <span>Total Item Dipilih : {{ ListItem.length }}</span>
+                    <span>Total Item Dipilih : {{ Selected }}</span>
                 </div>
                 <div class="col-12 cost">
                     <span>Total Harga : Rp. 1.000.000</span>
                 </div>
                 <div class="col-12 buy">
-                    <button class="btn-primary">Bayar Sekarang</button>
+                    <button class="btn-primary mr-2">Bayar Dengan Saldo</button>
+                    <button class="btn-primary">Bayar Dengan Metode Lain</button>
                 </div>
             </div>
         </div>
-        <div class="col-12 col-sm-12 card list-item" v-for="(item, i) in ListItem" :key="i">
-            <div class="select-wrapper">
-                <input class="form-check-input" type="checkbox" :name="`flexRadioDefault${ i }`">
-            </div>
-            <div class="images-wrapper">
-                <div class="images"></div>
-            </div>
-            <div class="item-wrapper">
-                <div class="wc-item">
-                    <h4 class="title">Name File Here..</h4>
-                    <p class="desc">bla bla bla bla</p>
-                    <p class="date">Date : 17 Agustus 1945</p>
-                </div>
-                <div class="wc-qty-done">
-                    <span>{{ 1 }} Qty</span>
-                </div>
-                <div class="wc-price">
-                    <span>Rp. 10.000.000</span>
+        <div class="col-12">
+            <div class="row" v-if="ResultData != null">
+                <div class="col-12 col-sm-12 card list-item" v-for="(order, i) in ResultData" :key="i">
+                    <div class="select-wrapper">
+                        <input class="form-check-input" type="checkbox" :name="`flexRadioDefault${ i }`">
+                    </div>
+                    <div class="images-wrapper">
+                        <div class="images"></div>
+                    </div>
+                    <div class="item-wrapper">
+                        <div class="wc-item">
+                            <h4 class="title">{{ order.product.name }}</h4>
+                            <!-- <p class="desc">bla bla bla bla</p> -->
+                            <p class="date">Date : {{ `${ this.$store.state.Tools.ChangeDateString(order.insertDate.substring(0, 10)) } ${ order.insertDate.substring(11, 20) }` }}</p>
+                        </div>
+                        <div class="wc-qty-done">
+                            <span>{{ order.quantity }} Qty</span>
+                        </div>
+                        <div class="wc-price">
+                            <span>{{ 'Rp.' + this.$store.state.Tools.PriceFormat(order.value, 2, ',', '.') }}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -46,22 +51,31 @@
         name: 'PembayaranTab',
         data() {
             return {
-                ListItem: [0,0,0,0,0,0]
+                Selected: 0,
+                DateFrom: this.$store.state.Tools.DateNowString(),
+                DateTo: this.$store.state.Tools.DateNowString(),
+                Token: `Bearer ${ this.$store.state.Login.UserData.token }`,
+                ResultData: null,
             }
         },
+        
         mounted() {
-
+            this.getDataAll(this.DateFrom, this.DateTo)
         },
 
         methods: {
+            dateFromChange(e) { this.DateFrom = e.target.value },
+            dateToChange(e) { this.DateTo = e.target.value },
+            searchData() {
+                this.getDataAll(this.DateFrom, this.DateTo)
+            },
             async getDataAll(date1, date2) {
                 let config = {
                     url: `https://dev-be.kompasdata.id/api/ShoppingCarts?startperiode=${ date1 }&endperiode=${ date2 }`,
                     headers: { Authorization: this.Token }
                 }
                 let AllData = await Axios(config)
-                console.log(AllData)
-                if ( AllData ) this.ResultData = AllData.data.filter(x => x.status === 1 )
+                if ( AllData ) this.ResultData = AllData.data.filter(x => x.status === 1)
                 else console.log(AllData)
             }
         }
