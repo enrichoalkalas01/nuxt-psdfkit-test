436c0c7ee4329619d23bf2e157bd6e7815120077
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-06-25 17:15:32 +0700

    update sementara dashboard sidebar

--- src/components/dashboard/SideBar.vue
+++ src/components/dashboard/SideBar.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="col-3 box-sidebar">
-        <div class="row d-flex flex-column flex-shrink-0 py-3 text-white bg-dark h-100">
+        <div class="row d-flex flex-column flex-shrink-0 py-3 text-white bg-dark h-100 wrap-sb">
             <!-- <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
                 <span class="fs-4">Dashboard</span>
