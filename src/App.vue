64ae2186873f890bb5b5ba2b9ee95084be4fb895
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-07-07 04:33:15 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -102,6 +102,7 @@
             async getRefreshTokenData() {
                 try {
                     let getRefreshTokenFromCookie = await Axios('https://data-api-dev.kompas.id/api/Login/kompas-token-refresh', { withCredentials: true })
+                    console.log(getRefreshTokenFromCookie)
                     return getRefreshTokenFromCookie.data
                 } catch(err) {
                     console.log(err)
