930c3c8116bdcb1c85e1d6a9870d79bf850c1a0d
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-07-07 08:39:52 +0700

    Update MainNav.vue

--- src/components/headers/MainNav.vue
+++ src/components/headers/MainNav.vue
@@ -81,7 +81,8 @@
                             </div>
                             <div class="wrapper-user" v-if="!this.$store.state.Login.LoginStatus">
                                 <!-- <a href="https://account.kompas.id/login?next=https://data-dev.kompas.id/" class="btn btn-line m-2" id="login">LOGIN</a> -->
-                                <a href="https://account.kompas.id/login?next=https://data-dev.kompas.id/" class="btn btn-line m-2" id="login">LOGIN/REGISTER</a>
+                                <!-- <a href="https://account.kompas.id/login?next=https://data-dev.kompas.id/" class="btn btn-line m-2" id="login">LOGIN/REGISTER</a> -->
+                                <a href="https://account.kompas.id/login?next=https://data-dev.kompas.id/" class="btn btn-second" id="login">LOGIN/REGISTER</a>
                                 <!-- <a href="/login" class="btn btn-line m-2" id="login">LOGIN</a> -->
                                 <!-- <a href="https://account.kompas.id/register?next=https://data-dev.kompas.id/" class="btn btn-second" id="register">DAFTAR</a> -->
                             </div>
