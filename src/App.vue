02f3e86656aff89f0bb106e9b4c6a49787e6ea64
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-07-11 14:40:49 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,7 @@
         },
 
         async mounted() {
-            // this.autoLoginSSO()
+            this.autoLoginSSO()
             /*
                 async function testRefresh() {
                     fetch('https://data-api-dev.kompas.id/api/Login/kompas-token-refresh', { credentials: "same-origin" })
