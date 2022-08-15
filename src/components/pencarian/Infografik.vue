acf5d96784543c457a7177159f8f1730a45568e6
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-08-15 17:10:55 +0700

    finishing part 1 infografik zoom in and out thumbnail

--- src/components/pencarian/Infografik.vue
+++ src/components/pencarian/Infografik.vue
@@ -11,15 +11,32 @@
                 <div class="row">
                     <div class="col-sm-4 my-3">
                         <!-- <a :href="`/infografik-detail/${ infografik.document_id }${ linkBack }`"> -->
-                            <!-- <div v-on:click="onMouseOverImages" class="images-div" :style="`background-image: url('${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }')`"></div> -->
                             <div
                                 @mouseover="onMouseOverImages"
                                 @mouseleave="onMouseLeaveImages"
                                 class="images-div"
                                 :id="`images-div-id-${ infografik.document_id }`"
+                                :dataImages="`${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }`"
+                                :style="`background-image: url('${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }')`"
                             >
-                                <img :src="`${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }`" alt="" class="infografik-image" />
+                                <img
+                                    :src="`${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }`"
+                                    alt="" class="infografik-image"
+                                    :id="`images-div-id-${ infografik.document_id }-image`"
+                                />
                             </div>
+                            <!-- <div
+                                @mouseover="onMouseOverImages"
+                                @mouseleave="onMouseLeaveImages"
+                                class="images-div"
+                                :id="`images-div-id-${ infografik.document_id }`"
+                            >
+                                <img
+                                    :src="`${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }`"
+                                    alt="" class="infografik-image"
+                                    style="display: none;"
+                                />
+                            </div> -->
                             <!-- <div class="images-div">
                                 <img v-on:click="onMouseOverImages" :src="`${ this.$store.state.Tools.GetUrlFiles + infografik.thumbnail }`" alt="" class="infografik-image w-100">
                             </div> -->
@@ -50,10 +67,7 @@
 <script>
     export default {
         name: 'Infografik',
-        props: [ 
-            'dataInfografiks', 'totalSearch'
-        ],
-
+        props: [ 'dataInfografiks', 'totalSearch' ],
         data (){
             return{
                 linkBack: null,
@@ -85,37 +99,70 @@
 
 
         methods: {
+            // async onMoveImages() {
+            //     console.log('here images..')
+            // },
+
             async onMouseOverImages(el) {
-                this.magnify(
-                    el.target.getAttribute('id'),
-                    el.target.clientWidth,
-                    el.target.clientHeight, 3
-                )
+                el.preventDefault()
+                // console.log(el.target.getAttribute('dataImages'))
+                // Get Real Elements
+                var boxImages = el.target
+                // var hiddenImages = document.querySelector(`#${ el.target.getAttribute('id') } img`)
+                
+                // Create Element Preview
+                let createPreview = document.createElement('div')
+                createPreview.setAttribute('id', `${ el.target.getAttribute('id') }-preview`)
+                createPreview.setAttribute('class', `images-preview-infografik`)
+                createPreview.style.width = `${ el.target.clientWidth }px`
+                createPreview.style.height = `${ el.target.clientHeight }px`
+                createPreview.style.backgroundColor = `#000`
+                createPreview.style.position = 'absolute'
+                createPreview.style.borderRadius = '10px'
+                createPreview.style.left = `${ el.target.clientWidth + 25 }px`
+                createPreview.style.top = '0px'
+                createPreview.style.backgroundPosition = 'center'
+                createPreview.style.backgroundSize = '125%'
+                createPreview.style.backgroundImage = `url('${ el.target.getAttribute('dataImages') }')`
+
+                // Append Preview
+                let imagesPreview = document.querySelectorAll(`.images-preview-infografik`)
+                if ( imagesPreview.length <= 0 ) boxImages.append(createPreview)
+                else console.log('images-preview has created!')
+
+                // Insert Preview Listener
+                boxImages.addEventListener('mousemove', (me) => {
+                    let pos = getCursorPos(me)
+                    let x = pos.x, y = pos.y
+
+                    console.log(x, y)
+                    console.log(`Width : ${ el.target.clientWidth }px`)
+                    console.log(`Width Percentage : ${ (x / el.target.clientWidth) * 100 }%`)
+                    console.log(`Height : ${ el.target.clientHeight }px`)
+                    console.log(`Height Percentage : ${ (y / el.target.clientHeight) * 100 }%`)
+                    
+                    let preview = document.querySelector(`#${ me.target.getAttribute('id') } .images-preview-infografik`)
+                    preview.style.backgroundPosition = `${ (y / el.target.clientHeight) * 100 }% ${ (x / el.target.clientWidth) * 100 }%`
+                })
+                
+                // Get Cursor Position
+                function getCursorPos(e) {
+                    var a, x = 0, y = 0
+                    e = e || window.event
+                    /* Get the x and y positions of the image: */
+                    a = boxImages.getBoundingClientRect()
+                    /* Calculate the cursor's x and y coordinates, relative to the image: */
+                    x = e.pageX - a.left
+                    y = e.pageY - a.top
+                    /* Consider any page scrolling: */
+                    x = x - window.pageXOffset
+                    y = y - window.pageYOffset
+                    return {x : x, y : y}
+                }
             },
 
             async onMouseLeaveImages() {
-                let magnifierGlass = document.querySelectorAll(".img-magnifier-glass")
-                if ( magnifierGlass.length > 0 ) magnifierGlass.forEach(el => el.remove())
-            },
-
-            magnify(imgID, wBox, hBox, zoom) {
-                console.log(imgID, zoom)
-                let boxImages = document.getElementById(imgID)
-                let widthBox = wBox, heightBox = hBox
-                console.log(widthBox, heightBox)
-
-                console.log(boxImages)
-
-                let boxPreview = document.createElement('div')
-                boxPreview.style.width = '175px'
-                boxPreview.style.height = '175px'
-                boxPreview.style.position = 'absolute'
-                boxPreview.style.backgroundColor = "#dedede"
-                boxPreview.style.left = wBox + 25 + 'px'
-                boxPreview.style.top = '0px'
-                
-
-                boxImages.insertBefore(boxPreview, document.querySelector(`#${ imgID } img`))
+                document.querySelectorAll('.images-preview-infografik').forEach(el => el.remove() )
             }
         }
     }
@@ -141,5 +188,17 @@
         max-height: 100%;
         height: 100%;
         width: auto !important;
+    }
+
+    .images-div .images-preview {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-size: 300%;
+        background-color: #574c4c;
+        background-position: left 0 top 0;
+        top: 0;
+        right: -10px;
+        display: none;
     }
 </style>
