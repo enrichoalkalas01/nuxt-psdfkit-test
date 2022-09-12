3190071da39f63ea9e4d038464b87cd50e425405
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-09-12 14:49:11 +0700

    update finish changes to lazy loads router #3

--- src/router/index.js
+++ src/router/index.js
@@ -5,28 +5,28 @@ import Activation from '../components/login/Activation.vue'
 // import ArtikelDetail from '../components/artikel/MainDetail.vue'
 // import FotoDetail from '../components/foto/MainDetail.vue'
 // import InfografikDetail from '../components/infografik/MainDetail.vue'
-import Buku from '../components/buku/MainPage.vue'
-import BukuDetail from '../components/buku/MainDetail.vue'
-import Data from '../components/data/MainPage.vue'
-import DataDetail from '../components/data/MainDetail.vue'
-import Layanan from '../components/layanan/MainPage.vue'
-import Bantuan from '../components/bantuan/MainPage.vue'
-import FAQ from '../components/bantuan/FAQ.vue'
-import PetaSitus from '../components/bantuan/PetaSitus.vue'
-import Pencarian from '../components/pencarian/MainPage.vue'
-import PencarianLanjut from '../components/pencarian/MainDetail.vue'
-import Test from '../components/test/MainPage.vue'
+// import Buku from '../components/buku/MainPage.vue'
+// import BukuDetail from '../components/buku/MainDetail.vue'
+// import Data from '../components/data/MainPage.vue'
+// import DataDetail from '../components/data/MainDetail.vue'
+// import Layanan from '../components/layanan/MainPage.vue'
+// import Bantuan from '../components/bantuan/MainPage.vue'
+// import FAQ from '../components/bantuan/FAQ.vue'
+// import PetaSitus from '../components/bantuan/PetaSitus.vue'
+// import Pencarian from '../components/pencarian/MainPage.vue'
+// import PencarianLanjut from '../components/pencarian/MainDetail.vue'
+// import Test from '../components/test/MainPage.vue'
 
 // Dashboard
-import Dashboard from '../components/dashboard/MainPage'
-import HomepageDashboard from '../components/dashboard/Dashboard.vue'
-import TopUp from '../components/payments/TopUp.vue'
-import Voucher from '../components/payments/Voucher.vue'
-import ShoppingCart from '../components/dashboard/Shopping/ShoppingCart.vue'
-import Chatting from '../components/dashboard/Chat/Chatting.vue'
-import Syarat from '../components/dashboard/Syarat.vue'
-import Account from '../components/dashboard/Account.vue'
-import History from '../components/dashboard/History/MainPage.vue'
+// import Dashboard from '../components/dashboard/MainPage'
+// import HomepageDashboard from '../components/dashboard/Dashboard.vue'
+// import TopUp from '../components/payments/TopUp.vue'
+// import Voucher from '../components/payments/Voucher.vue'
+// import ShoppingCart from '../components/dashboard/Shopping/ShoppingCart.vue'
+// import Chatting from '../components/dashboard/Chat/Chatting.vue'
+// import Syarat from '../components/dashboard/Syarat.vue'
+// import Account from '../components/dashboard/Account.vue'
+// import History from '../components/dashboard/History/MainPage.vue'
 
 const routes = [
     { path: '/notification-activation', name: 'notification', component: Notification },
@@ -45,42 +45,39 @@ const routes = [
     { path: '/infografik-detail/:id', name: 'infografik-detail', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/infografik/MainDetail.vue`) },
     
     // Buku
-    { path: '/buku', name: 'buku', component: Buku },
-    { path: '/buku-detail/:id', name: 'buku-detail', component: BukuDetail },
+    { path: '/buku', name: 'buku', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/buku/MainPage.vue`) },
+    { path: '/buku-detail/:id', name: 'buku-detail', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/buku/MainDetail.vue`) },
 
     // Data
-    { path: '/data', name: 'data', component: Data },
-    { path: '/data-detail/:collection/:id', name: 'data-detail', component: DataDetail },
+    { path: '/data', name: 'data', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/data/MainPage.vue`) },
+    { path: '/data-detail/:collection/:id', name: 'data-detail', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/data/MainDetail.vue`) },
 
     // Layanan
-    { path: '/layanan', name: 'layanan', component: Layanan },
+    { path: '/layanan', name: 'layanan', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/layanan/MainPage.vue`) },
 
     // About or Bantuan
-    { path: '/bantuan', name: 'bantuan', component: Bantuan },
-    { path: '/FAQ', name: 'faq', component: FAQ },
-    { path: '/peta-situs', name: 'peta-situs', component: PetaSitus },
+    { path: '/bantuan', name: 'bantuan', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/bantuan/MainPage.vue`) },
+    { path: '/FAQ', name: 'faq', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/bantuan/FAQ.vue`) },
+    { path: '/peta-situs', name: 'peta-situs', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/bantuan/PetaSitus.vue`) },
 
     // Pencarian
-    { path: '/pencarian', name: 'pencarian', component: Pencarian },
-    { path: '/pencarian-lanjut', name: 'pencarian-lanjut', component: PencarianLanjut },
-
-    // Tester
-    { path: '/test', name: 'test', component: Test },
+    { path: '/pencarian', name: 'pencarian', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/pencarian/MainPage.vue`) },
+    { path: '/pencarian-lanjut', name: 'pencarian-lanjut', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/pencarian/MainDetail.vue`) },
 
     // Dashboard
     {
-        path: '/dashboard', name: 'dashboard', component: Dashboard,
+        path: '/dashboard', name: 'dashboard', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/MainPage`),
         children: [
-            { path: '/dashboard', name: 'dashboard', component: HomepageDashboard },
-            { path: '/dashboard/topup', name: 'topup', component: TopUp },
-            { path: '/dashboard/kupon', name: 'voucher', component: Voucher },
-            { path: '/dashboard/daftar-pesanan', name: 'daftar-pesanan', component: ShoppingCart },
-            { path: '/dashboard/daftar-transaksi', name: 'daftar-transaksi', component: History },
-            { path: '/dashboard/pesan', name: 'chatting', component: Chatting },
-            { path: '/dashboard/syarat', name: 'syarat', component: Syarat },
-            { path: '/dashboard/akun', name: 'account', component: Account },
+            { path: '/dashboard', name: 'dashboard', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/Dashboard.vue`) },
+            { path: '/dashboard/topup', name: 'topup', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/payments/TopUp.vue`) },
+            { path: '/dashboard/kupon', name: 'voucher', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/payments/Voucher.vue`) },
+            { path: '/dashboard/daftar-pesanan', name: 'daftar-pesanan', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/Shopping/ShoppingCart.vue`) },
+            { path: '/dashboard/daftar-transaksi', name: 'daftar-transaksi', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/History/MainPage.vue`) },
+            { path: '/dashboard/pesan', name: 'chatting', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/Chat/Chatting.vue`) },
+            { path: '/dashboard/syarat', name: 'syarat', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/Syarat.vue`) },
+            { path: '/dashboard/akun', name: 'account', component: import(/* webpackChunkName: "lazy-view-[request]" */ `../components/dashboard/Account.vue`) },
         ]
-    },
+    }
 ]
 
 const router = createRouter({
