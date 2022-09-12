3df9b03939e21ee229f43df282418cee509179bd
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2022-09-12 14:31:09 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,28 +2,28 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const webpack = require('webpack')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
-      new BundleAnalyzerPlugin(),
-    ],
+  // configureWebpack: {
+  //   plugins: [
+  //     new BundleAnalyzerPlugin(),
+  //   ],
 
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "vendors",
-            chunks: "all"
-          }
-        }
-      }
-    },
-  },
+  //   optimization: {
+  //     splitChunks: {
+  //       cacheGroups: {
+  //         commons: {
+  //           test: /[\\/]node_modules[\\/]/,
+  //           name: "vendors",
+  //           chunks: "all"
+  //         }
+  //       }
+  //     }
+  //   },
+  // },
 
   devServer: {
     disableHostCheck: true,
   },
 
-  lintOnSave: process.env.NODE_ENV !== 'production',
-  publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/production-sub-path/' : '/',
 }
