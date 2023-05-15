678b9402d475eaadab1661f0ee644fe5a46ab63d
Enricho Alkalas <20742166+enrichoalkalas01@users.noreply.github.com>
2023-05-16 06:16:21 +0700

    add pdf nuxt 3 template

---
+++ components/PSPDFKitContainer.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div class="pdf-container"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PSPDFKitContainer',
+        props: {
+            pdfFile: { type: String, required: true, },
+        },
+        PSPDFKit: null,
+
+        mounted() {
+            this.loadPSPDFKit().then((instance) => {
+                this.$emit("loaded", instance)
+            })
+        },
+
+        watch: {
+            pdfFile(val) {
+                if (val) {
+                    this.loadPSPDFKit()
+                }
+            },
+        },
+
+        methods: {
+            async loadPSPDFKit() {
+                import("pspdfkit").then(PSPDFKit => {
+                    this.PSPDFKit = PSPDFKit
+                    this.PSPDFKit.unload(".pdf-container")
+                    this.PSPDFKit.load({
+                        document: this.pdfFile,
+                        container: ".pdf-container",
+                        baseUrl: "http://localhost:3000/"
+                    })
+                })
+                // import("pspdfkit").then((PSPDFKit) => {
+                //     this.PSPDFKit = PSPDFKit
+                //     PSPDFKit.unload(".pdf-container")
+                //         return PSPDFKit.load({
+                //             document: this.pdfFile,
+                //             container: ".pdf-container",
+                //             baseUrl: "http://localhost:7788/",
+                //         })
+                //     })
+                // .catch((error) => {
+                //     console.error(error)
+                // })
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .pdf-container {
+        height: 100vh
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>tester</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+        data() {
+            return {
+                tester: 'asd'
+            }
+        },
+
+        asyncData() {
+            
+        },
+
+        mounted() {
+            import('pspdfkit').then(response => {
+                console.log(response)
+            })
+        }, 
+    }
+</script>
